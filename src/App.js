078a5c71ddcb0ad963abbeb255944f82ab2db7fb
eslazyk10 <eslazy32@gmail.com>
2021-05-24 16:20:14 -0500

    created Coin.js and imported data from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import Coin from "./Coin";
 import axios from "axios";
 import './App.css';
 
 function App() {
   const [ coins, setCoins ] = useState([]);
+  const [ search, setSearch ] = useState("");
   
   const apiUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
@@ -12,15 +14,35 @@ function App() {
       setCoins(res.data);
     }).catch(error => alert("Error"));
   }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  }
+
+  const filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()));
   
   return (
     <div className="App">
       <div className="coin-search">
         <h1 className="coint-text">Search a Currency</h1>
         <form>
-          <input type="text" placeholder="Search" className="coin-input" />
+          <input type="text" placeholder="Search" className="coin-input" onChange={ handleChange }/>
         </form>
       </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={ coin.id }
+            name={ coin.name }
+            image={ coin.image }
+            symbol={ coin.symbol }
+            price={coin.current_price}
+            marketcap={ coin.market_cap }
+            priceChange={ coin.price_change_percentage_24h }
+            volume={coin.total_volume}
+          />
+        );
+      })}
     </div>
   );
 }
